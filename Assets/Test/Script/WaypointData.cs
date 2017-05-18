a818dfae1482bcd1e777c89194247596c894b8a3
Skynet-Chris <ccparr13@gmail.com>
2017-05-18 21:28:49 +0000

    A scriptable object for storing closed path waypoints

---
+++ Assets/Test/Script/WaypointData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaypointData", menuName = "Define Waypoints")]
+public class WaypointData : ScriptableObject {
+
+    public List<Transform> Waypoints;
+
+}
