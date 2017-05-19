8b4f74024fc0ccc0cbc588ac62e2add5cc07f888
Skynet-Chris <ccparr13@gmail.com>
2017-05-19 02:55:57 +0000

    path marker + prefab

---
+++ Assets/Test/Script/Editor/PathConfigEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathConfig))]
+public class PathConfigEditor : Editor {
+    [SerializeField]
+    private IndicatorSphere m_indicatorPrefab;
+    private GameObject m_indicatorParent;
+    private List<IndicatorSphere> m_pathMarkers;
+
+    public override void OnInspectorGUI()
+    {
+        m_pathMarkers = new List<IndicatorSphere>();
+        m_indicatorParent = new GameObject("PathIndicators");
+
+        DrawDefaultInspector();
+        EditorGUILayout.LabelField("Some Label");
+        if (GUILayout.Button("Spawn New Waypoint"))
+        {
+            Instantiate<IndicatorSphere>(m_indicatorPrefab, m_indicatorParent.transform);
+            //pathBuilder.SpawnWaypoint();
+        }
+    }
+}
---
+++ Assets/Test/Script/IndicatorSphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorSphere : MonoBehaviour {
+
+    private Color m_color = Color.yellow;
+    public Color SphereColor {get{ return m_color;} set{ m_color = value; m_renderer.material.color = m_color;  }}
+
+    private MeshRenderer m_renderer;
+
+    private void Awake()
+    {
+        m_renderer = gameObject.GetComponent<MeshRenderer>();
+        Reset();
+    }
+
+    public void Reset()
+    {
+        SphereColor = Color.yellow;
+    }
+}
