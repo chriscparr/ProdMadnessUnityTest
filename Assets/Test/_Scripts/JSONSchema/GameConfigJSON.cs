d165faea64253f04d983b71362116c61d5c13756
Skynet-Chris <ccparr13@gmail.com>
2017-05-20 17:31:35 +0000

    Created json schemas for the game data. Demonstrated correct parsing with a test script + prefab that prints game config and player entries to the console

---
+++ Assets/Test/_Scripts/JSONSchema/GameConfigJSON.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class GameConfigJSON
+{
+    public int lapsNumber;
+    public int playersInstantiationDelay;
+}
