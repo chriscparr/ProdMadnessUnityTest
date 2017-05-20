1fd053672316df2c69cbf40964286919a8a3b3b5
Skynet-Chris <ccparr13@gmail.com>
2017-05-20 14:04:52 +0000

    Basic editor utility to build/view paths and save to scriptable object

--- Assets/Test/Config/PathConfig.cs
+++ Assets/Test/Config/PathConfig.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Assets/Test/Config/PathData.asset", menuName = "Create Path")]
 public class PathConfig : ScriptableObject
 {
-
+    public Vector3[] PathPoints;
 }
--- Assets/Test/Script/Editor/PathConfigEditor.cs
+++ Assets/Test/Script/Editor/PathConfigEditor.cs
@@ -7,20 +7,109 @@ using UnityEditor;
 public class PathConfigEditor : Editor {
     [SerializeField]
     private IndicatorSphere m_indicatorPrefab;
+
     private GameObject m_indicatorParent;
-    private List<IndicatorSphere> m_pathMarkers;
+    private PathConfig m_pathConfig;
+
+    
+    public List<IndicatorSphere> m_pathMarkers = new List<IndicatorSphere>();
+
+    private void OnEnable()
+    {
+        m_pathConfig = (PathConfig)target;
+        if (GameObject.Find("IndicatorParent") != null)
+        {
+            m_indicatorParent = GameObject.Find("IndicatorParent");
+        }
+        else
+        {
+            m_indicatorParent = new GameObject("IndicatorParent");
+        }
+        CreatePathFromScriptableObject();
+    }
+
+    /*
+    private void OnDestroy()
+    {
+        RemoveIndicators();
+        DestroyImmediate(m_indicatorParent);
+    }
+    */
 
     public override void OnInspectorGUI()
     {
-        m_pathMarkers = new List<IndicatorSphere>();
-        m_indicatorParent = new GameObject("PathIndicators");
-
         DrawDefaultInspector();
-        EditorGUILayout.LabelField("Some Label");
-        if (GUILayout.Button("Spawn New Waypoint"))
+
+        if (GUILayout.Button("Spawn new waypoint"))
         {
-            Instantiate<IndicatorSphere>(m_indicatorPrefab, m_indicatorParent.transform);
-            //pathBuilder.SpawnWaypoint();
+            IndicatorSphere test;
+            test = Instantiate(m_indicatorPrefab, m_indicatorParent.transform);
+            if (m_pathMarkers.Count > 1)
+            {
+                Vector3 next = m_pathMarkers[m_pathMarkers.Count - 1].transform.position - m_pathMarkers[m_pathMarkers.Count - 2].transform.position;
+                test.transform.position = m_pathMarkers[m_pathMarkers.Count - 1].transform.position + next;
+            }
+            else
+            {
+                test.transform.position = Vector3.one;
+            }
+            m_pathMarkers.Add(test);
+            SavePathToScriptableObject();
+
+        }
+        if (GUILayout.Button("Remove last waypoint"))
+        {
+            if(m_pathMarkers.Count > 0)
+            {
+                DestroyImmediate(m_pathMarkers[m_pathMarkers.Count - 1].gameObject);
+                m_pathMarkers.RemoveAt(m_pathMarkers.Count - 1);
+                SavePathToScriptableObject();
+            }
+        }
+        if (GUILayout.Button("Clear all waypoints"))
+        {
+            RemoveIndicators();
+        }
+        /*
+        if (GUILayout.Button("BIG_BUTTON"))
+        {
+            Debug.Log("<color=#ffff00>Big button pressed!</color>");
+            IndicatorSphere test;
+            test = Instantiate(m_indicatorPrefab);
+        }
+        */
+    }
+
+    private void SavePathToScriptableObject()
+    {
+        Vector3[] configPathPoints = new Vector3[m_pathMarkers.Count];
+        for(int i = 0; i < m_pathMarkers.Count; i++)
+        {
+            configPathPoints[i] = m_pathMarkers[i].transform.position;
+        }
+        m_pathConfig.PathPoints = configPathPoints;
+    }
+    
+    private void CreatePathFromScriptableObject()
+    {
+        if (m_pathConfig.PathPoints.Length > 0)
+        {
+            RemoveIndicators();
+            for (int i = 0; i < m_pathConfig.PathPoints.Length; i++)
+            {
+                m_pathMarkers.Add(Instantiate(m_indicatorPrefab, m_indicatorParent.transform));
+                m_pathMarkers[i].transform.position = m_pathConfig.PathPoints[i];
+            }
         }
     }
+
+    private void RemoveIndicators()
+    {
+       while(m_indicatorParent.transform.childCount > 0)
+        {
+            DestroyImmediate(m_indicatorParent.transform.GetChild(0).gameObject);
+        }
+        m_pathMarkers.Clear();
+    }
+    
 }
