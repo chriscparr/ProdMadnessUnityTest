90e8df62e492ccff1292967a97175b37850f5f07
Skynet-Chris <ccparr13@gmail.com>
2017-05-30 22:19:50 +0000

    Created simple path navigation script which guides the car around the track. Needs refining though.

--- Assets/Test/_Scripts/CarController.cs
+++ Assets/Test/_Scripts/CarController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarController : MonoBehaviour {
 
     private Rigidbody m_carRigidBody;
+    private int m_waypointIndex = 0;
     [SerializeField]
     private WheelCollider m_frontLeft;
     [SerializeField]
@@ -32,20 +33,36 @@ public class CarController : MonoBehaviour {
     [SerializeField]
     private MeshCollider m_carCollider;
 
+
     private void Awake()
     {
         m_carRigidBody = GetComponent<Rigidbody>();
         m_carRigidBody.centerOfMass = m_centerOfMass;
     }
+
+    private void Navigate()
+    {
+        if(gameObject.transform.position != m_pathConfig.PathPoints[m_waypointIndex])
+        {
+            Vector3 waypoint = m_pathConfig.PathPoints[m_waypointIndex];
+
+           //s m_steer = Mathf.Tan()
+
+        }
+        else
+        {
+            m_waypointIndex++;
+        }
+    }
 	
 	// Update is called once per frame
-	void Update ()
+	private void FixedUpdate ()
     {
         m_power = 0f;
         m_brake = 0f;
         m_steer = 0f;
 
-        m_power = Input.GetAxis("Vertical") * m_enginePower * Time.deltaTime * 250f;
+        m_power = Input.GetAxis("Vertical") * m_enginePower * Time.fixedDeltaTime * 250f;
         m_steer = Input.GetAxis("Horizontal") * m_steerMaxAngle;
 
         m_frontLeft.steerAngle = m_steer;
---
+++ Assets/Test/_Scripts/PathNavigator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNavigator : MonoBehaviour {
+
+    private Rigidbody m_carRigidBody;
+    private int m_waypointIndex = 0;
+    [SerializeField]
+    private PathConfig m_pathConfig;
+    [SerializeField]
+    private int m_speed = 20;
+    private int m_laps = 4;
+
+    private void Awake()
+    {
+        m_carRigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+		if(m_laps > 0)
+        {
+            if(m_waypointIndex < m_pathConfig.PathPoints.Length)
+            {
+                //turn + move
+                transform.forward = Vector3.RotateTowards(transform.forward, m_pathConfig.PathPoints[m_waypointIndex] - transform.position, m_speed * Time.deltaTime, 0.0f);
+                transform.position = Vector3.MoveTowards(transform.position, m_pathConfig.PathPoints[m_waypointIndex], m_speed * Time.deltaTime);
+
+                if (transform.position == m_pathConfig.PathPoints[m_waypointIndex])
+                {
+                    m_waypointIndex++;
+                }
+            }
+            else
+            {
+                m_waypointIndex = 0;
+                m_laps--;
+            }
+
+        }
+
+
+	}
+}
