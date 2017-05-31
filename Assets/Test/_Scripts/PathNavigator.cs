fe4ba2701201d780e72a9183a9295dc0e667fe58
Skynet-Chris <ccparr13@gmail.com>
2017-05-31 23:34:38 +0000

    Created minimised path, set car as nav agent, tested 2 agents overtaking - great success!

--- Assets/Test/_Scripts/PathNavigator.cs
+++ Assets/Test/_Scripts/PathNavigator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PathNavigator : MonoBehaviour {
 
@@ -10,38 +11,44 @@ public class PathNavigator : MonoBehaviour {
     private PathConfig m_pathConfig;
     [SerializeField]
     private int m_speed = 20;
-    private int m_laps = 4;
+    private int m_laps;
+
+    private NavMeshAgent m_agent;
 
     private void Awake()
     {
         m_carRigidBody = GetComponent<Rigidbody>();
+        m_agent = GetComponent<NavMeshAgent>();
+        m_agent.autoBraking = false;
     }
-	
-	// Update is called once per frame
-	private void Update ()
+
+    private void Start()
     {
-		if(m_laps > 0)
+        GotoNextPoint();
+    }
+
+    private void GotoNextPoint()
+    {
+        m_waypointIndex++;
+        if (m_waypointIndex < m_pathConfig.PathPoints.Length)
         {
-            if(m_waypointIndex < m_pathConfig.PathPoints.Length)
-            {
-                Vector3 waypoint = new Vector3(m_pathConfig.PathPoints[m_waypointIndex].x, transform.position.y, m_pathConfig.PathPoints[m_waypointIndex].z);
-                //turn + move
-                transform.forward = Vector3.RotateTowards(transform.forward, waypoint - transform.position, m_speed * Time.deltaTime, 0.0f);
-                transform.position = Vector3.MoveTowards(transform.position, waypoint, m_speed * Time.deltaTime);
-
-                if (transform.position == waypoint)     //m_pathConfig.PathPoints[m_waypointIndex])
-                {
-                    m_waypointIndex++;
-                }
-            }
-            else
-            {
-                m_waypointIndex = 0;
-                m_laps--;
-            }
-
+            m_agent.destination = m_pathConfig.PathPoints[m_waypointIndex];
         }
+        else
+        {
+            m_waypointIndex = 0;
+            m_laps++;
+            m_agent.destination = m_pathConfig.PathPoints[m_waypointIndex];
+        }
+        //Debug.Log("<color=#ffff00>Going to wapoint " + m_waypointIndex.ToString() + "</color>");
+    }
 
-
-	}
+    private void Update()
+    {
+        if (m_agent.remainingDistance < 13f)
+        {
+            //Debug.Log("<color=#ffff00>Going to next wapoint!</color>");
+            GotoNextPoint();
+        }
+    }
 }
