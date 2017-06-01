7f1eab96a8029b394959509d092a42e3b0435c5b
Skynet-Chris <ccparr13@gmail.com>
2017-06-01 19:25:54 +0000

    car now with race % progress property - will be used in ui later on

--- Assets/Test/_Scripts/GameManager.cs
+++ Assets/Test/_Scripts/GameManager.cs
@@ -6,8 +6,9 @@ public class GameManager : MonoBehaviour {
     [SerializeField]
     private PathNavigator m_pathNavPrefab;
 
+    private PlayerConfigJSON[] m_playerConfigs;
+    public PlayerConfigJSON[] PlayerConfigs { get { return m_playerConfigs; }set { m_playerConfigs = value; } }
 
-    //public CarController m_carController;
     private List<PathNavigator> m_racers = new List<PathNavigator>();
 
 	// Use this for initialization
@@ -20,7 +21,11 @@ public class GameManager : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.Space))
         {
             m_racers.Add(Instantiate<PathNavigator>(m_pathNavPrefab));
-            Debug.Log("<color=#44ff00>Racer no " + m_racers.Count.ToString() + " GO!</color>");
+            PlayerConfigJSON dummyConfig = new PlayerConfigJSON();
+            dummyConfig.Name = "Test_" + (m_racers.Count - 1).ToString();
+            dummyConfig.Velocity = Random.Range(20, 40);
+            m_racers[m_racers.Count - 1].PlayerConfig = dummyConfig;
+            Debug.Log("<color=#44ff00>Racer " + m_racers[m_racers.Count - 1].PlayerConfig.Name + " GO!</color>");
         }
 	}
 }
