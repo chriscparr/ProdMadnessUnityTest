08e9f56754d135a5c5f842600b43184bb2a19dc3
Skynet-Chris <ccparr13@gmail.com>
2017-06-01 21:37:30 +0000

    Cars now auto spawning using random json player config objects sorted by velocity

--- Assets/Test/_Scripts/GameManager.cs
+++ Assets/Test/_Scripts/GameManager.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
 
 public class GameManager : MonoBehaviour {
     [SerializeField]
     private PathNavigator m_pathNavPrefab;
 
-    private PlayerConfigJSON[] m_playerConfigs;
-    public PlayerConfigJSON[] PlayerConfigs { get { return m_playerConfigs; }set { m_playerConfigs = value; } }
+    //private PlayerConfigJSON[] m_playerConfigs;
+   // public PlayerConfigJSON[] PlayerConfigs { get { return m_playerConfigs; }set { m_playerConfigs = value; } }
 
     private List<PathNavigator> m_racers = new List<PathNavigator>();
+    private DataJSON m_gameData;
+    private List<PlayerConfigJSON> m_playerConfigs = new List<PlayerConfigJSON>();
+    private string m_jsonData;
+    private string m_path = "Assets/StreamingAssets/Data.txt";
 
-    // Use this for initialization
+    private void Awake()
+    {
+        StreamReader reader = new StreamReader(m_path, true);
+        m_jsonData = reader.ReadToEnd();
+        reader.Close();
+
+        m_gameData = JsonUtility.FromJson<DataJSON>(m_jsonData);
+
+        Debug.Log("GameConfig - laps = " + m_gameData.GameConfiguration.lapsNumber.ToString() + " delay = " + m_gameData.GameConfiguration.playersInstantiationDelay.ToString());
+        foreach (PlayerConfigJSON p in m_gameData.Players)
+        {
+            Debug.Log("Player - Name:" + p.Name + " Velocity:" + p.Velocity + " Color:" + p.Color + " Icon:" + p.Icon);
+        }
+    }
+    
+
     private void Start ()
     {
         BeginRace();
@@ -20,19 +41,32 @@ public class GameManager : MonoBehaviour {
 
     private void BeginRace()
     {
-        for(int i = 0; i < 8; i++)
+        m_playerConfigs.Clear();
+        for (int i = 0; i < 8; i++)
         {
-            Invoke("SpawnCar", 1f * i);
+            m_playerConfigs.Add(m_gameData.Players[Random.Range(0, m_gameData.Players.Length)]);
+        }
+        m_playerConfigs = m_playerConfigs.OrderByDescending(o => o.Velocity).ToList();
+        //m_playerConfigs.Sort();
+
+        int secs = m_gameData.GameConfiguration.playersInstantiationDelay / 1000;
+        
+        for (int i = 0; i < 8; i++)
+        {
+            Invoke("SpawnCar", secs * i);
         }
     }
 
     private void SpawnCar()
     {
         m_racers.Add(Instantiate<PathNavigator>(m_pathNavPrefab));
+        /*
         PlayerConfigJSON dummyConfig = new PlayerConfigJSON();
         dummyConfig.Name = "Test_" + (m_racers.Count - 1).ToString();
         dummyConfig.Velocity = Random.Range(20, 40);
         m_racers[m_racers.Count - 1].PlayerConfig = dummyConfig;
+        */
+        m_racers[m_racers.Count - 1].PlayerConfig = m_playerConfigs[m_racers.Count - 1];
         m_racers[m_racers.Count - 1].StartRace();
         Debug.Log("<color=#44ff00>Racer " + m_racers[m_racers.Count - 1].PlayerConfig.Name + " GO!</color>");
     }
--- Assets/Test/_Scripts/JSONSchema/PlayerConfigJSON.cs
+++ Assets/Test/_Scripts/JSONSchema/PlayerConfigJSON.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
-public class PlayerConfigJSON {
+public class PlayerConfigJSON : IComparable {
     public string Name;
     public int Velocity;
     public string Color;
-    public string Icon;	
+    public string Icon;
+    public int CompareTo(object obj)
+    {
+        return Velocity.CompareTo(obj);
+    }
 }
