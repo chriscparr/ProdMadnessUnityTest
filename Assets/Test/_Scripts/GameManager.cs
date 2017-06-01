bcd6af3a18efbd3d730c342b70f5c98f23c86de1
Skynet-Chris <ccparr13@gmail.com>
2017-06-01 00:10:38 +0000

    Spawning cars with space bar

--- Assets/Test/_Scripts/GameManager.cs
+++ Assets/Test/_Scripts/GameManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    [SerializeField]
+    private PathNavigator m_pathNavPrefab;
 
-    public CarController m_carController;
 
+    //public CarController m_carController;
+    private List<PathNavigator> m_racers = new List<PathNavigator>();
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +17,10 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_racers.Add(Instantiate<PathNavigator>(m_pathNavPrefab));
+            Debug.Log("<color=#44ff00>Racer no " + m_racers.Count.ToString() + " GO!</color>");
+        }
 	}
 }
